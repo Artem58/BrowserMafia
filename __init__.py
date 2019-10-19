c7b13b64c3ae11457abf0e13606eac17c1dad1d2
Artem58 <artyom.yablochko@bk.ru>
2019-10-19 15:19:55 +0400

    Project structure

---
+++ __init__.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', serverTime=str(datetime.now()).split('.')[0])
+
+
+@app.route('/signUp')
+def signUp():
+    return render_template('signUp.html')
+
+
+@app.route('/chooseGame')
+def chooseGame():
+    return render_template('chooseGame.html')
+
+
+@app.route('/createGame')
+def createGame():
+    return render_template('createGame.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 80)
